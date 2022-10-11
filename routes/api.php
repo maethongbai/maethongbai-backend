5a5cb98746179f018ce49152ef789a10e4cc492d
Ratchathorn <ratchathorn.s@ku.th>
2022-10-11 11:45:59 +0700

    added user authentication & changed user to fit auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -40,3 +41,13 @@ Route::apiResources([
     "/wholesales" => \App\Http\Controllers\Api\WholesaleController::class,
     "/withdrawals" => \App\Http\Controllers\Api\WithdrawalController::class
 ]);
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
